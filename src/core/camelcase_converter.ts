690b393156d91590cdf2cf6c5bab40f497d0325a
Mario Pinto <mario@leanmind.es>
2023-01-24 23:13:42 +0000

    🟢 CamelCase converter should remove hyphens and underscores

--- src/core/camelcase_converter.ts
+++ src/core/camelcase_converter.ts
@@ -2,6 +2,9 @@ export function camelcase_converter(text: string) {
 	if (text.length == 0) {
 		return '';
 	}
+
+	text = text.replace('-', ' ');
+	text = text.replace('_', ' ');
 	const words = text.split(' ').map((word) => {
 		return word.toLowerCase() === word ? word.charAt(0).toUpperCase() + word.slice(1) : word;
 	});
