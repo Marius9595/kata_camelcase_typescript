d2b60cfd0befe919de0c3f04fe321feb6d336561
Mario Pinto <mario@leanmind.es>
2023-01-24 22:44:20 +0000

    🟢 CamelCase converter should capitalize lowercase words

--- src/core/camelcase_converter.ts
+++ src/core/camelcase_converter.ts
@@ -2,5 +2,8 @@ export function camelcase_converter(text: string) {
 	if (text.length == 0) {
 		return '';
 	}
+	if (text.toLowerCase() === text) {
+		return text.charAt(0).toUpperCase() + text.slice(1);
+	}
 	return text;
 }
