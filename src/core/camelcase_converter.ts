90ae5a7675cfd90d704516452772e146415cfd7f
Mario Pinto <mario@leanmind.es>
2023-01-24 23:18:54 +0000

    🔵 extract explaining variables for conditionals, results and literals

--- src/core/camelcase_converter.ts
+++ src/core/camelcase_converter.ts
@@ -3,10 +3,13 @@ export function camelcase_converter(text: string) {
 		return '';
 	}
 
-	text = text.replace('-', ' ');
-	text = text.replace('_', ' ');
-	const words = text.split(' ').map((word) => {
-		return word.toLowerCase() === word ? word.charAt(0).toUpperCase() + word.slice(1) : word;
+	const separator = ' ';
+	text = text.replace('-', separator);
+	text = text.replace('_', separator);
+	const words = text.split(separator).map((word) => {
+		const is_word_not_capitalized = word.toLowerCase() === word;
+		const word_capitalized = word.charAt(0).toUpperCase() + word.slice(1);
+		return is_word_not_capitalized ? word_capitalized : word;
 	});
 	return words.reduce((previousValue, currentValue) => `${previousValue}${currentValue}`, '');
 }
