a06f0f4fc418afe8175697a14a6bd8cc61975bac
Mario Pinto <mario@leanmind.es>
2023-01-24 23:28:03 +0000

    🔵 reducing complexity

--- src/core/camelcase_converter.ts
+++ src/core/camelcase_converter.ts
@@ -6,10 +6,6 @@ export function camelcase_converter(text: string) {
 	const separator = ' ';
 	text = text.replace('-', separator);
 	text = text.replace('_', separator);
-	const words = text.split(separator).map((word) => {
-		const is_word_not_capitalized = word.toLowerCase() === word;
-		const word_capitalized = word.charAt(0).toUpperCase() + word.slice(1);
-		return is_word_not_capitalized ? word_capitalized : word;
-	});
-	return words.reduce((previousValue, currentValue) => `${previousValue}${currentValue}`, '');
+	const words = text.split(separator).map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+	return words.join('');
 }
